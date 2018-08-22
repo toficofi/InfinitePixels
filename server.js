afcd2ed270be7958800356bd0d5ded69a5c29217
Jishaxe <josh@joshbe.me>
2018-08-22 05:56:27 +0100

    attempt fix of server failing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let viewingDistance = 40
 let chunkSize = 16
 let port = 80
 let worldSize = 5000
-let connectToLocalhost = false
+let connectToLocalhost = true
 console.log("Infinite Pixels server loading...")
 if (!fs.existsSync("world")) fs.mkdirSync("world")
 
@@ -136,6 +136,10 @@ function endConnection(socket) {
 
 function processPacket(data, socket) {
     let identifier = currentPacketInfo.ident
+    if (identifier !== 0 && !socket.client) {
+        console.log("Client tried to send packets before authing")
+        return
+    }
 
     switch (identifier) {
         case 0: 
